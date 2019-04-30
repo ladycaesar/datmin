5043ec23c2ad67ee651b42f9fd59fa30bdc710f1
ladycaesar <sevikalady@gmail.com>
2019-05-01 03:50:18 +0700

    Add files via upload

---
+++ fragment/footer.php
@@ -0,0 +1,19 @@
+<!-------------FOOOOOOOOOOOOOOOOOOOOOTER----------------------------->
+                <div class="footer">
+                    <div class="legal pure-g">
+
+                        <div class="pure-u-1 pure-u-sm-1-2">
+
+                            <p class="legal-copyright">
+                                &copy; 2019 Asuransi Sinar Mas Bintaro.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div><!-- div main -->
+        </div>
+        <!--div layout -->
+
+    </body>
+</html>
---
+++ fragment/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="icon" type="image/jpg" href="img/snrms.jpg">
+        <title>Simas Sehat Bintaro</title>
+
+        <link rel="stylesheet" href="assets/pure-min.css">
+        <link rel="stylesheet" href="assets/baby-blue.css">
+        <link rel="stylesheet" href="assets/bootstrap.min.css">
+
+    </head>
+
+    <body>
