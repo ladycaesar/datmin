06da9830f8ba4207654f7447f259fa1b7c67cd3c
ladycaesar <sevikalady@gmail.com>
2019-05-01 03:50:01 +0700

    Create sidebar.php

---
+++ fragment/sidebar.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+<div id="layout">
+            <a href="#menu" id="menuLink" class="menu-link"> <span></span> </a>
+
+            <div id="menu">
+              <h3></h3>
+                <div class="pure-menu pure-menu-open">
+                    <a class="pure-menu-heading" href="">Menu</a>
+
+                    <ul>
+
+
+                        <li class=" ">
+                            <a href="home.php">Beranda</a>
+                        </li>
+                        <li class=" ">
+                            <a href="data_training.php">Data Training</a>
+                        </li>
+                        <li class=" ">
+                            <a href="data_testing.php">Data Testing</a>
+                        </li>
+                        <li class="dropdown"><a href="#">Algoritma C4.5</a>
+		                        <ul class="isi-dropdown">
+			                           <li><a href="c45.php">Pohon Keputusan</a></li>
+			                              <li><a href="#">Mining</a></li>
+		                                      </ul>
+	                                       </li>
+                        <li class=" ">
+                            <a href="">Pengujian</a>
+                        </li>
+                        <li class=" ">
+                            <a href="prediksi.php">Prediksi</a>
+                        </li>
+                        <li class=" ">
+                            <a href="about.php">Author</a>
+                        </li>
+                        <li class=" ">
+                            <a href="logout.php">Logout</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div><!--div menu -->
